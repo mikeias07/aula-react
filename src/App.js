0c0cf164ab22a4c1e178b1fd2c19fc64bb01e1d6
Mikeias Silva <mikeias07@hotmail.com>
2019-09-03 19:06:51 -0300

    cadastro autores e limpa campos

--- src/App.js
+++ src/App.js
@@ -1,53 +1,47 @@
 import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
-import $ from 'jquery';
-import InputCustomizado from './componentes/InputCustomizado';
-import BotaoCustomizado from './componentes/BotaoSubmitCustomizado';
 import AutorBox from './Autor';
-  //function App() { 
-  //assim que estava no inicio como function app
-  class App extends Component {
+//function App() { 
+//assim que estava no inicio como function app
+class App extends Component {
 
+  render() {
+    return (
 
+      <div id="layout">
 
+        <a href="#menu" id="menuLink" className="menu-link">
 
-    render() {
-      return (
+          <span></span>
+        </a>
 
-        <div id="layout">
+        <div id="menu">
+          <div className="pure-menu">
+            <a className="pure-menu-heading" href="http://localhost:3000">Company</a>
 
-          <a href="#menu" id="menuLink" className="menu-link">
-
-            <span></span>
-          </a>
-
-          <div id="menu">
-            <div className="pure-menu">
-              <a className="pure-menu-heading" href="#">Company</a>
-
-              <ul className="pure-menu-list">
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
-              </ul>
-            </div>
+            <ul className="pure-menu-list">
+              <li className="pure-menu-item"><a href="http://localhost:3000" className="pure-menu-link">Home</a></li>
+              <li className="pure-menu-item"><a href="http://localhost:3000" className="pure-menu-link">Autor</a></li>
+              <li className="pure-menu-item"><a href="http://localhost:3000" className="pure-menu-link">Livro</a></li>
+            </ul>
           </div>
-
-          <div id="main">
-            <div className="header">
-              <h1>Cadastro de Autores</h1>
-            </div>
-            <div className="content" id="content">
-              <AutorBox />
-
-            </div>
-          </div>
-
-
         </div>
 
-      );
-    }
+        <div id="main">
+          <div className="header">
+            <h1>Cadastro de Autores</h1>
+          </div>
+          <div className="content" id="content">
+            <AutorBox />
+
+          </div>
+        </div>
+
+
+      </div>
+
+    );
   }
+}
 export default App;
--- src/Autor.js
+++ src/Autor.js
@@ -1,48 +1,51 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
 import InputCustomizado from './componentes/InputCustomizado';
-import BotaoCustomizado from './componentes/BotaoSubmitCustomizado';
 import PubSub from 'pubsub-js';
-import TratadorErros from './TratadorErros';
+import TratadorErros from  './TratadorErros';
 
-class FormularioAutores extends Component {
-    constructor() {
-        super();
-        this.state = { nome: '', email: '', senha: '' };
-        this.enviaForm = this.enviaForm.bind(this);
-        this.setNome = this.setNome.bind(this);
-        this.setEmail = this.setEmail.bind(this);
-        this.setSenha = this.setSenha.bind(this);
-    }
+class FormularioAutor extends Component {
 
+  constructor() {
+    super();    
+    this.state = {nome:'',email:'',senha:''};
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
+  }
+  
     enviaForm(evento) {
         evento.preventDefault();
         console.log("dados sendo enviados");
         $.ajax({
-            //url: 'https://localhost:3000/api/autores',
-            url: 'https://cdc-react.herokuapp.com/api/autores',
+            url: 'http://localhost:8080/api/autores',
+            //url: 'https://cdc-react.herokuapp.com/api/autores',
             contentType: 'application/json',
-            cache: false,
+            // cache: false,
             dataType: 'json',
-            method: 'POST',
-
+            type: 'POST',
             data: JSON.stringify({
                 nome: this.state.nome,
                 email: this.state.email,
                 senha: this.state.senha
             }),
-            success: function (resposta) {
+            success: function (novaListagem) {
                 console.log("enviado com sucesso");
-                this.setState({ lista: resposta });
+                PubSub.publish('atualiza-lista-autores',novaListagem);
+                this.setState({ nome:'',email:'',senha:'' });
             }.bind(this),
             error: function (resposta) {
                 console.log(resposta);
-                console.log("errorasdasd");
+                if (resposta.status === 400) {
+                    new TratadorErros().publicaErros(resposta.responseJSON);
+                }
+            },
+            beforeSend: function () {
+                PubSub.publish("limpa-erros", {});
             }
         });
     }
-
-
 
     setNome(evento) {
         this.setState({ nome: evento.target.value });
@@ -60,9 +63,12 @@ class FormularioAutores extends Component {
             <div className="pure-form pure-form-aligned">
                 <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
                     <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome" />
-                    <InputCustomizado id="email" type="text" name="email" value={this.state.email} onChange={this.setEmail} label="Email" />
-                    <InputCustomizado id="senha" type="text" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha" />
-                    <BotaoCustomizado label="Salvar" />
+                    <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email" />
+                    <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha" />
+                    <div className="pure-control-group">
+                        <label></label>
+                        <button type="submit" className="pure-button pure-button-primary">Gravar</button>
+                    </div>
                 </form>
 
             </div>
@@ -70,7 +76,7 @@ class FormularioAutores extends Component {
     }
 }
 
-class TabelaAutores extends Component {
+class TabelaAutor extends Component {
     render() {
         return (
             <div>
@@ -83,7 +89,7 @@ class TabelaAutores extends Component {
                     </thead>
                     <tbody>
                         {
-                            this.state.lista.map(function (autor) {
+                            this.props.lista.map(function (autor) {
                                 return (
                                     <tr key={autor.id}>
                                         <td>{autor.nome}</td>
@@ -109,8 +115,8 @@ export default class AutorBox extends Component {
 
     componentDidMount() {
         $.ajax({
-            //url: 'https://localhost:3000/api/autores',
-            url: "https://cdc-react.herokuapp.com/api/autores",
+            url: 'http://localhost:8080/api/autores',
+            //url: "https://cdc-react.herokuapp.com/api/autores",
             dataType: 'json',
             success: function (resposta) {
                 this.setState({ lista: resposta });
@@ -123,10 +129,11 @@ export default class AutorBox extends Component {
             this.setState({ lista: novaLista });
         }.bind(this));
     }
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                <FormularioAutores lista={this.state.lista}/>
+                <FormularioAutor />
+                <TabelaAutor lista={this.state.lista} />
             </div>
         );
     }
